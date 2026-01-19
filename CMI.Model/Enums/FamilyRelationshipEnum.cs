6de36fc6a37892b6b44de7efca1f2d0f19312efa
106159633 <106159633@SYS-PC65>
2026-01-19 14:58:06 +0330

    added summary on enum

--- CMI.Model/Enums/FamilyRelationshipEnum.cs
+++ CMI.Model/Enums/FamilyRelationshipEnum.cs
@@ -2,16 +2,34 @@
 
 public enum FamilyRelationshipEnum
 {
+    /// <summary>
+    /// Mother
+    /// </summary>
     [Description("مادر")]
     Mother = 0,
+    /// <summary>
+    /// Parent
+    /// </summary>
     [Description("پدر")]
     Parent = 1,
+    /// <summary>
+    /// Brother
+    /// </summary>
     [Description("برادر")]
     Brother = 2,
+    /// <summary>
+    /// Sister
+    /// </summary>
     [Description("خواهر")]
     Sister = 3,
+    /// <summary>
+    /// GrandFather
+    /// </summary>
     [Description("پدر بزرگ")]
     GrandFather = 4,
+    /// <summary>
+    /// GrandMother
+    /// </summary>
     [Description("مادر بزرگ")]
     GrandMother = 5
 }
